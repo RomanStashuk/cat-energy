c5fa583082b8d2ff6c773facfd6790403c27cb4f
RomanStashuk <romanstashuk1@gmail.com>
2022-07-01 17:47:12 +0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,1 @@
+!function(){const n=document.querySelector(".main-nav"),a=document.querySelector(".main-nav__toggle");n.classList.remove("main-nav--nojs"),a.addEventListener("click",(function(){n.classList.contains("main-nav--closed")?(n.classList.remove("main-nav--closed"),n.classList.add("main-nav--opened")):(n.classList.add("main-nav--closed"),n.classList.remove("main-nav--opened"))}))}();
