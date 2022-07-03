c8465f6825b8174ef31e28de47061881cd4017d7
RomanStashuk <romanstashuk1@gmail.com>
2022-07-03 15:28:21 +0300

    small fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toggle');
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
 
-navToggle.addEventListener('click', function() {
+navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
